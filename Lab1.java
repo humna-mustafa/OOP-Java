54f112335bd4bb2d8b862d90403f4df58f4ca52d
Humna Mustafa <humnamustafa01@gmail.com>
2026-02-11 21:24:32 +0500

    First OOPS Program

---
+++ Lab1.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+public class Lab1{
+public static void main(String arg[]){
+System.out.println("Hello!!!!");
+System.out.printf("Humna Mustafa\n");
+System.out.print("How are you?");
+}
+}
