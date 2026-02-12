09da63c7ba1c215726df72cff7dcaa424f013b6c
Humna Mustafa <humnamustafa01@gmail.com>
2026-02-12 11:36:29 +0500

    adding ?

--- Lab1.java
+++ Lab1.java
@@ -3,6 +3,6 @@ public class Lab1{
 public static void main(String arg[]){
 System.out.println("Hello!!!!");
 System.out.printf("Humna Mustafa\n");
-System.out.print("How are you???");
+System.out.print("How are you??");
 }
 }
