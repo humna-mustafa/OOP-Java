62031fa66d7e287f77dd435e8e4ecf915504d84c
Humna Mustafa <humnamustafa01@gmail.com>
2026-02-12 12:49:14 +0500

    improve file formatting

--- Lab1.java
+++ Lab1.java
@@ -1,19 +1,23 @@
 import java.lang.*;
 public class Lab1{
 public static void main(String arg[]){
-//System.out.println("Hello!!!!");
-//System.out.printf("Humna Mustafa\n");
-//System.out.print("How are you??");
-        int number = 0;
 
-        if (number > 0) {
-            System.out.println("The number is positive");
-        } else if (number < 0) {
-            System.out.println("The number is negative");
-        } else {
-            System.out.println("The number is zero");
-        }
+//System.out.println("Hi!!");
+//System.out.printf("I'm Humna Mustafa\n");
+//System.out.print("BS Software Engineering Student");
+//System.out.print("FA25-BSE-051");
+ 
+// Comparison Program       
 
+int number = 5;
+if (number>0) 
+  System.out.println("The number is positive");
+else if (number<0) 
+  System.out.println("The number is negative");
+ else 
+  System.out.println("The number is zero");
+        
+// Traingle Pattern
 
 for(int i=1;i<10;i++){
 for (int j=1; j<=i;j++){
