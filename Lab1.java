fbc695c503a79597c60e12eb7324a18da2e1505f
Humna Mustafa <humnamustafa01@gmail.com>
2026-02-12 12:22:54 +0500

    if program

--- Lab1.java
+++ Lab1.java
@@ -1,8 +1,18 @@
 import java.lang.*;
 public class Lab1{
 public static void main(String arg[]){
-System.out.println("Hello!!!!");
-System.out.printf("Humna Mustafa\n");
-System.out.print("How are you??");
+//System.out.println("Hello!!!!");
+//System.out.printf("Humna Mustafa\n");
+//System.out.print("How are you??");
+        int number = 0;
+
+        if (number > 0) {
+            System.out.println("The number is positive");
+        } else if (number < 0) {
+            System.out.println("The number is negative");
+        } else {
+            System.out.println("The number is zero");
+        }
+
 }
 }
